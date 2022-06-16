8e69e5a34fb87cf9ae312ff22f497dcd2bdb1acf
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-06-16 16:01:36 -0600

    added questions array

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown.js')
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'projectTitle'
+    },
+    {
+        type: 'input',
+        message: 'Give a description of your project?',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'githubName'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email'
+    },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then(response => {
+            const userInput = generateMarkdown(response)
+            console.log(userInput)
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,10 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectTitle}
 
+# Description
+${data.description}
 `;
 }
 
