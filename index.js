056f6c3ec82d9c92288c5481114603b9f38606e9
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-06-20 14:08:41 -0600

    added if/else statements for licenses

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ const questions = [
                 value: '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)',
             },
             {
-                name: 'The Do What the F*ck You Want to Public License',
-                value: '[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)',
+                name: 'Apache',
+                value: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
             },
             {
                 name: 'None',
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,4 @@
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+//This function returns the license link, that the user selected, in the table of contents; If there is no license, returns an empty string.
 function renderLicenseLink(license) {
   if (license === ''){
     return "";
@@ -8,21 +7,40 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+//This function returns the license section of README; If there is no license, returns an empty string.
 function renderLicenseSection(license) {
   let nameLicense;
   if (license === '') {
     return "";
-  } else if (license === '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)')
-  {
-    nameLicense = "Boost";
   }
+  else if (license === '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)') {
+    nameLicense = "MIT";
     return `## License
     This project is licensed under ${nameLicense} license.`
-}
+  }
+  else if (license === '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)') {
+    nameLicense = "Boost";
+    return `## License
+    This project is licensed under ${nameLicense} license.`
+  }
+  else if (license === '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)') {
+    nameLicense = "IBM";
+    return `## License
+    This project is licensed under ${nameLicense} license.`
+  }
+  else if (license === '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)') {
+    nameLicense = "Mozilla";
+    return `## License
+    This project is licensed under ${nameLicense} license.`
+  }
+  else if (license === '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)') {
+    nameLicense = "Apache";
+    return `## License
+    This project is licensed under ${nameLicense} license.`
+  }
+};
 
-// TODO: Create a function to generate markdown for README
+//This function generates the markdown README file.
 function generateMarkdown(data) {
   return `# ${data.projectTitle}
   ${data.license}
